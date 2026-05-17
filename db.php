08d3a567e79ddd5f53f5f50acefb5fbb389d26f2
Ruhaan Chaudhary <ruhaanchaudhary@Ruhaans-MacBook-Air.local>
2026-05-17 05:46:50 +0530

    Connect to Railway database

--- db.php
+++ db.php
@@ -1,15 +1,17 @@
 <?php
 declare(strict_types=1);
 
-$dbHost = "127.0.0.1";
-$dbUser = "root";
-$dbPass = "";
-$dbName = "co_po_system";
+$dbHost = mysql.railway.internal;
+$dbUser = root;
+$dbPass = vijkiUKmRcXwvilSWRJkocJLtRFkTdUI;
+$dbName = railway;
+$dbPort = (int)3306;
 
-$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
+?>
